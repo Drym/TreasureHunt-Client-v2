00973e71070a353ab31c37131f2ff59f8dee7f6b
Lucas SAUVAGE <sl200103@etu.unice.fr>
2017-01-12 11:01:03 +0100

    Récupération des données du formulaire

--- public/js/controller/firstPageController.js
+++ public/js/controller/firstPageController.js
@@ -1,3 +1,13 @@
 app.controller("firstCtrl", function($scope){
 	$scope.test = "test";
+
+	$scope.submit = submitFunc;
+
+	function submitFunc() {
+
+		var pseudo = document.getElementById("pseudo").value;
+		var team = document.getElementById("team").value;
+
+		console.log(pseudo, team);
+	}
 });
