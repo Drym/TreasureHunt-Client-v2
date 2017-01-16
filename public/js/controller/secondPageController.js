76f7d25a155e3e1271a3604f2a47d49c50475892
Lucas SAUVAGE <sl200103@etu.unice.fr>
2017-01-16 17:12:10 +0100

    modal img fullscreen onclick

--- public/js/controller/secondPageController.js
+++ public/js/controller/secondPageController.js
@@ -117,7 +117,23 @@ app.controller("secondCtrl", function($scope, $http) {
         // or server returns response with an error status.
         console.log(response);
     });
+
+
+    //Full screen img
+    $('#Fullscreen').css('height', $(document).outerWidth() + 'px');
+    //for when you click on an image
+    $('#img-enigma').click(function () {
+        var src = $(this).attr('src'); //get the source attribute of the clicked image
+        $('#Fullscreen img').attr('src', src); //assign it to the tag for your fullscreen div
+        $('#Fullscreen').fadeIn();
+    });
+    $('#Fullscreen').click(function () {
+        $(this).fadeOut(); //this will hide the fullscreen div if you click away from the image.
+    });
+
+
 });
+
 
 
 
