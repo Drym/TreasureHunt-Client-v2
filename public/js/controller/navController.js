3ab819db6dbb85dc1ca822616d8a91fba7c43cd2
Donzelle <theo.donzelle@gmail.com>
2017-01-19 09:56:56 +0100

    Activation carte si connecté

---
+++ public/js/controller/navController.js
@@ -0,0 +1,3 @@
+app.controller("navCtrl", function($scope, socketFactory){
+	$scope.socketFactory = socketFactory;
+});
--- public/js/factory/factory.js
+++ public/js/factory/factory.js
@@ -1,44 +1,45 @@
 app.factory('socketFactory', function($rootScope){
 	var socketFactory = {};
 
-	socketFactory.isConnected = true;
+	socketFactory.isConnected = false;
 	socketFactory.isEnigme = false;
 
 	var socket = io('http://10.212.99.100:8080');
 
-	 socketFactory.sendNameTeam = function(name, team) {
-	 	socket.emit('newUser', {'name' : name, 'team' : team});
-	 };
+	socketFactory.sendNameTeam = function(name, team) {
+		socket.emit('newUser', {'name' : name, 'team' : team});
+	};
 
-	 socketFactory.sendPosition = function(position) {
-	 	socket.emit('sendPosition', position);
-	 };
+	socketFactory.sendPosition = function(position) {
+		socket.emit('sendPosition', position);
+	};
 
-	 socketFactory.sendAnswer = function(answer) {
-	 	//socketFactory.isEnigme
-	 	socket.emit('sendAnswer', answer)
-	 };
+	socketFactory.sendAnswer = function(answer) {
+		//socketFactory.isEnigme
+		socket.emit('sendAnswer', answer)
+	};
 
-	 socket.on('connexion', function(isConnected) {
-	 	socketFactory.isConnected = isConnected;
-	 })
+	socket.on('connexion', function(isConnected) {
+		socketFactory.isConnected = isConnected;
+	})
 
-	 socket.on('enigme', function(data) {
-	 	socketFactory.isEnigme = true;
-	 	console.log('enigme : ' + JSON.stringify(data))
-	 });
+	socket.on('enigme', function(data) {
+		socketFactory.isEnigme = true;
+		console.log('enigme : ' + JSON.stringify(data))
+	});
 
-	 socket.on('areas', function(data) {
-	 	console.log('areas : ' + JSON.stringify(data));
+	socket.on('areas', function(data) {
+		console.log('areas : ' + JSON.stringify(data));
 
-		 socketFactory.areas = data;
-	 });
+		socketFactory.areas = data;
+	});
 
 	socket.on('response', function(data) {
 		console.log('response : ' + data); //ok ou ko
 
 		if(data == "ok") {
 			$rootScope.$broadcast('response-ok');
+			socketFactory.isConnected = false;
 		}
 	});
 
