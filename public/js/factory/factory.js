aec14b965f11549b832bb4ff9a40cdfc0ce185a5
Donzelle <theo.donzelle@gmail.com>
2017-01-19 09:35:37 +0100

    pour merge

--- public/js/factory/factory.js
+++ public/js/factory/factory.js
@@ -4,33 +4,35 @@ app.factory('socketFactory', function(){
 	socketFactory.isConnected = true;
 	socketFactory.isEnigme = false;
 
-	var socket = io('http://10.212.116.203:8080');
+	var socket = io('http://localhost:8080');
 
-	// socketFactory.sendNameTeam = function(name, team) {
-	// 	socket.emit('newUser', {'name' : name, 'team' : team});
-	// };
+	socket.emit('connectEvent', {});
 
-	// socketFactory.sendPosition = function(position) {
-	// 	socket.emit('sendPosition', position);
-	// };
+	socketFactory.sendNameTeam = function(name, team) {
+		socket.emit('newUser', {'name' : name, 'team' : team});
+	};
 
-	// socketFactory.sendAnswer = function(answer) {
-	// 	socketFactory.isEnigme
-	// 	socket.emit('sendAnswer', answer)
-	// };
+	socketFactory.sendPosition = function(position) {
+		socket.emit('sendPosition', position);
+	};
 
-	// socket.on('connexion', function(isConnected) {
-	// 	socketFactory.isConnected = isConnected;
-	// })
+	socketFactory.sendAnswer = function(answer) {
+		socketFactory.isEnigme
+		socket.emit('sendAnswer', answer)
+	};
 
-	// socket.on('enigme', function(data) {
-	// 	socketFactory.isEnigme = true;
-	// 	console.log('enigme : ' + JSON.stringify(data))
-	// });
+	socket.on('connexion', function(isConnected) {
+		socketFactory.isConnected = isConnected;
+	})
 
-	// socket.on('areas', function(data) {
-	// 	console.log('areas : ' + JSON.stringify(data));
-	// });
+	socket.on('enigme', function(data) {
+		socketFactory.isEnigme = true;
+		console.log('enigme : ' + JSON.stringify(data))
+	});
+
+	socket.on('areas', function(data) {
+		console.log('areas : ' + JSON.stringify(data));
+	});
 
 	return socketFactory;
 });
