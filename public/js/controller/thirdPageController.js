5d9da1cee7126a7ff3cb16b158dda6af79b52f8c
Lucas SAUVAGE <sl200103@etu.unice.fr>
2017-02-06 14:08:15 +0100

    Ajout de coms

--- public/js/controller/thirdPageController.js
+++ public/js/controller/thirdPageController.js
@@ -1,12 +1,20 @@
 app.controller("thirdCtrl", function($scope, $rootScope, socketFactory, chatFactory) {
-	$scope.chatFactory = chatFactory;
 
+
+	$scope.chatFactory = chatFactory;
 	$scope.myName = socketFactory.name;
 
+    /**
+     * Envoie un message
+     * @param message
+     */
 	$scope.sendMessage = function(message) {
 		socketFactory.sendMessage({'user': socketFactory.name, 'content' : message});
-	}
+	};
 
+    /**
+     * Scroll automatiquement en bas de la page
+     */
 	function scrollBottom() {
 		var chat = document.getElementById("messages");
 		if(chat) {
@@ -14,6 +22,9 @@ app.controller("thirdCtrl", function($scope, $rootScope, socketFactory, chatFact
 		}
 	}
 
+    /**
+     * Reception d'un nouveau message
+     */
 	$rootScope.$on('newMessage', function(event, data) {
 		scrollBottom();
 	})
